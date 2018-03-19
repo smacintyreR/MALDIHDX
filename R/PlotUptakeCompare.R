1cd6411934890006cef76e117be664feaaad3f69
smacintyreR <sam.macintyre.sac@gmail.com>
2018-03-19 16:24:06 +1100

    Commented code and applied Google R style guide

--- R/PlotUptakeCompare.R
+++ R/PlotUptakeCompare.R
@@ -1,46 +1,40 @@
-PlotUptakeCompare <- function(pepnum,pepIDS = peptide.identifications,AllCents,times,Conditions = c("Bound","Unbound")){
+PlotUptakeCompare <- function(pepnum, pep.ids = peptide.identifications,
+                              all.cents, times,
+                              conditions = c("Bound","Unbound")){
 
+  # Separate centroids based on experimental conditions
+  rep.no <- length(all.cents)/2
+  cents.condit.1 <- all.cents[1:rep.no]
+  cents.condit.2 <- all.cents[ (rep.no + 1):2*rep.no]
     
-  NoRep <- length(AllCents)/2
-  CentsCondit1 <- AllCents[1:NoRep]
-  CentsCondit2 <- AllCents[(NoRep+1):2*NoRep]
-  
-  AvCentsCondit1 <- AvCent(CentsCondit1)
-  AvCentsCondit2 <- AvCent(CentsCondit2)
-
-  
-  times <- as.numeric(as.vector(CentsCondit1[[1]][,1]))
+  # Average centroids across technical replicates
+  av.cents.condit.1 <- AvCent(cents.condit.1)
+  av.cents.condit.2 <- AvCent(cents.condit.2)
+    
+  # Extract time points
+  times <- as.numeric(as.vector(cents.condit.1[[1]][, 1]))
   times[1] <- 1
-  noTimes <- length(times)
-
-
-  # Add the theoretical undeuterated
-  #UnCent <- pepIDS[pepnum,18]
-
-  # HDX Uptake values
-  #CentsCond1Mod <- rbind(as.data.frame(CentsCondit1[,2]),UnCent)
-
-  UptakesCond1 <- AvCentsCondit1 - AvCentsCondit1[1,]
-
-  # HDX Uptake values
-  
-  
-  #CentsCond2Mod <- rbind(as.data.frame(CentsCondit2[,2]),UnCent)
-
-  UptakesCond2 <- AvCentsCondit2 - AvCentsCondit2[1,]
-
-  xmax <- times[noTimes] + 3
-  YmaxTh <- pepIDS[pepnum,15]
-
-  YvalueMax <- max(YmaxTh,max(UptakesCond2))
-  # Plotting
-  plot(times,UptakesCond1[,1],log='x',ylim = c(0,YvalueMax),col = "blue",pch=4,ylab="Relative Deuterium Uptake (AMU)",xlab = "Time (seconds)",main = paste("Back Exchange Peptide",as.character(pepnum),"Flag Binding",sep="_"),type="b")
-  points(times,UptakesCond2[,1],col="red",pch=3,type="b")
-
-  abline(h = YmaxTh,col="blue",lty =2)
-  text(times[noTimes]-3555, YmaxTh - 0.25, "Theoretical Maximum Uptake", col = "blue",cex = 0.75)
-
-  legend('topright',Conditions,
-         pch=c(3,4),col=c('red','blue'))
-
+  no.times <- length(times)
+    
+  # Calculate uptake values from centroids
+  uptakes.cond.1 <- av.cents.condit.1 - av.cents.condit.1[1, ]
+  uptakes.cond.2 <- av.cents.condit.2 - av.cents.condit.2[1, ]
+    
+  # Set plot dimensions
+  x.max <- times[no.times] + 3
+  y.max.th <- pep.ids[pepnum, 15]
+  y.value.max <- max(y.max.th, max(uptakes.cond.2))
+    
+  # Plot the uptake curves 
+  plot(times, uptakes.cond.1[, 1], log = 'x',ylim = c(0, y.value.max), 
+       col = "blue", pch = 4, ylab = "Relative Deuterium Uptake (AMU)",
+       xlab = "Time (seconds)", main = paste("Back Exchange Peptide",
+    as.character(pepnum), "Flag Binding", sep="_"), type = "b")
+  points(times,uptakes.cond.2[,1],col="red",pch=3,type="b")
+  abline(h = y.max.th, col="blue", lty=2)
+    
+  # Add label and legend
+  text(times[no.times] - 3555, y.max.th - 0.25, "Theoretical Maximum Uptake", 
+  col = "blue", cex = 0.75)
+  legend('topright', conditions, pch=c(3,4), col=c('red','blue'))
 }
