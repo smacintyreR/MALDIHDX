8706dad34432f80af273bae5be34f5fa93829902
smacintyreR <sam.macintyre.sac@gmail.com>
2018-07-09 13:00:17 +1000

    Edited MEMHDX functions to be flexible with the number of replicates

--- R/MEMHDX_functions.R
+++ R/MEMHDX_functions.R
@@ -54,72 +54,6 @@ MEMHDXall <- function(AllpeptideCentroidsU = peptide.featuresU,
 # function which combines the peptide.identification information and centroids
 # for one peptide
 
-MEMHDXformat <- function(Centroids,IDs,pepno,bound = TRUE){
-    
-    
-    # Number of technical replicates
-    noRep <- length(Centroids)
-    
-    # Number of Timepoints
-    noTime <- dim(Centroids[[1]])[1]
-    
-    
-    # Create 0 matrix with appropriate dimensions
-    CentroidMatrix <- matrix(0,noTime*noRep,5)
-    CentroidMatrix <- as.data.frame(CentroidMatrix)
-    
-    k = 0
-    for(i in 1:noTime){
-        for(j  in 1:noRep){
-            k = k+1
-            # Fill MEMHDX style matrix
-            CentroidMatrix[k,4] <- j
-            CentroidMatrix[k,5] <- Centroids[[j]][i,2]
-            CentroidMatrix[k,2] <- as.character(Centroids[[j]][i,1])
-            
-        }
-        
-    }
-    
-    
-    # Make this more flexible to accommodate more states?
-    if(bound == T){
-        state <- "BOUND"
-    }
-    
-    else{
-        state <- "UNBOUND"
-    }
-    
-    stateVec <- rep.row(state, noTime*noRep)
-    
-    
-    # Replicate ID information
-    IDsPep <- IDs[pepno,]
-    namesID <-colnames(IDsPep)
-    IDsPep <- rep.row(IDsPep,noTime*noRep)
-    
-    # Extract charge column
-    charge <- IDsPep[,5]
-    IDsPep <- IDsPep[,-5]
-    
-    # Create final matrix
-    IDsPep <- as.data.frame(IDsPep)
-    colnames(IDsPep) <- namesID[-5]
-    CentroidMatrix[,1] <- stateVec
-    CentroidMatrix[,3] <- charge
-    
-    # Set column names
-    colnames(CentroidMatrix) <- c("State","Exposure","z","Replicate","Center")
-    
-    
-    
-    # Combine identifications and centroids
-    FullMatrix <- cbind(IDsPep,CentroidMatrix)
-    
-    return(FullMatrix)
-    
-}
 
 
 
@@ -153,16 +87,18 @@ Idmodify <- function(IDs = peptide.identifications){
 # Order: Unbound, Unbound, Unbound, 
 
 
-MEMHDXformat <- function(Centroids,IDs,pepno,bound = TRUE){
+MEMHDXformat <- function(Centroids,IDs,pepno,bound = TRUE,noTimePoints,NoReplicates){
     
     
     # Number of technical replicates
-    noRep <- length(Centroids)
+    noRep <- NoReplicates
     
     # Number of Timepoints
-    noTime <- dim(Centroids[[1]])[1]
+    noTime <- noTimePoints
     
     # Create 0 matrix with appropriate dimensions
+    
+    
     CentroidMatrix <- matrix(0,noTime*noRep,5)
     CentroidMatrix <- as.data.frame(CentroidMatrix)
     
@@ -171,9 +107,23 @@ MEMHDXformat <- function(Centroids,IDs,pepno,bound = TRUE){
         for(j  in 1:noRep){
             k = k+1
             # Fill MEMHDX style matrix
+            
+            if(is.null(Centroids[[j]][i,2])==T){
+                CentroidMatrix[k,5] <- NA
+            }
+            
+            else{
+                CentroidMatrix[k,5] <- Centroids[[j]][i,2]
+            }
             CentroidMatrix[k,4] <- j
-            CentroidMatrix[k,5] <- Centroids[[j]][i,2]
+            
+            if(is.null(Centroids[[j]][i,1]) == T){
+                CentroidMatrix[k,2] <- NA 
+            }
+            
+            else{
             CentroidMatrix[k,2] <- as.character(Centroids[[j]][i,1])
+            }
             
         }
     }
@@ -245,24 +195,27 @@ MEMHDXall2 <- function(AllpeptideCentroids,
     # Number of peptides to analyse
     noFeatures <- length(AllpeptideCentroids)
     
-    CentroidsU <- lapply(AllpeptideCentroids,function(x) x[1:3])
-    CentroidsB <- lapply(AllpeptideCentroids,function(x) x[4:6])
+    noRep <- length(AllpeptideCentroids[[1]])/2
+    noTime <- dim(AllpeptideCentroids[[1]][[1]])[1]
+    
+    CentroidsU <- lapply(AllpeptideCentroids,function(x) x[1:noRep])
+    CentroidsB <- lapply(AllpeptideCentroids,function(x) x[(noRep+1):(2*noRep)])
     
     # Builds up MEMHDX formatted matrix feature by feature - untreated
-    New <- MEMHDXformat(CentroidsU[[1]],IDs = IDs,1,bound=F)
+    New <- MEMHDXformat(CentroidsU[[1]],IDs = IDs,1,bound=F,noTimePoints = noTime,NoReplicates = noRep)
     for(i in 2:noFeatures){
         
-        nextPep <- MEMHDXformat(CentroidsU[[i]],IDs = IDs,i,bound = FALSE)
+        nextPep <- MEMHDXformat(CentroidsU[[i]],IDs = IDs,i,bound=F,noTimePoints = noTime,NoReplicates = noRep)
         New <- rbind.data.frame(New,nextPep)
         
     }
     
     
     # Builds up MEMHDX formatted matrix feature by feature - treated
-    New2 <- MEMHDXformat(CentroidsB[[1]],IDs = IDs,1)
+    New2 <- MEMHDXformat(CentroidsB[[1]],IDs = IDs,1,bound=T,noTimePoints = noTime,NoReplicates = noRep)
     for(i in 2:noFeatures){
         
-        nextPep <- MEMHDXformat(CentroidsB[[i]],IDs = IDs,i,bound = TRUE)
+        nextPep <- MEMHDXformat(CentroidsB[[i]],IDs = IDs,i,bound=T,noTimePoints = noTime,NoReplicates = noRep)
         New2<- rbind.data.frame(New2,nextPep)
         
     }
